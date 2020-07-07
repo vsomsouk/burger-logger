f0167029f968d261c0583b326b4ddcec239e05b2
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-07-06 22:03:30 -0500

    created table for schema file and added values for seeds sql file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', true);
+
+SELECT * FROM burgers;
