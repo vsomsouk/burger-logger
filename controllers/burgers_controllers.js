09f387f5f5f863dc8402078fc45d7a70ef8760ae
Vanida Somchaleunsouk <vsomsouk@gmail.com>
2020-09-05 15:19:11 -0500

    added gitignore file. set up server to connect. files imported

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,8 +1,18 @@
-//Inside the burgers_controller.js file, import the following:
-
-//Express
-//burger.js
-
+//Inside the burgers_controller.js file, import the following: express, router, burger.js (models)
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger")
 
 
 //Create the router for the app, and export the router at the end of your file
+
+//GET
+
+//POST
+
+//PUT
+
+//DELETE
+
+//Export routes for server.js to use
+module.exports = router;
--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-//handlebars
-cont exphbs = require("./controllers/burgers_controllers.js");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controllers");
 
+app.use(routes);
 
-
-
-
-
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
